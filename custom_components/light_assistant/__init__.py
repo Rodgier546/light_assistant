a634f6fdcaee89027f18a9c2af0de5e2ee3471f5
Rodgier546 <157311652+Rodgier546@users.noreply.github.com>
2024-08-09 21:59:59 +0100

    Update __init__.py

--- custom_components/light_assistant/__init__.py
+++ custom_components/light_assistant/__init__.py
@@ -1,1 +1,23 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "light")
+    )
+    
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    if await hass.config_entries.async_forward_entry_unload(entry, "light"):
+        hass.data[DOMAIN].pop(entry.entry_id)
+        return True
+    return False

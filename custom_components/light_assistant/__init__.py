56c30d784c3af400883db9275ca719114e80aaab
Rodgier546 <157311652+Rodgier546@users.noreply.github.com>
2024-08-09 22:31:14 +0100

    Update __init__.py

--- custom_components/light_assistant/__init__.py
+++ custom_components/light_assistant/__init__.py
@@ -1,25 +1,21 @@
-from homeassistant.config_entries import ConfigEntry
+import logging
 from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Light Assistant component."""
+    _LOGGER.debug("Setting up Light Assistant")
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(hass: HomeAssistant, entry):
     """Set up Light Assistant from a config entry."""
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
-
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "light")
-    )
-    
+    _LOGGER.debug(f"Setting up Light Assistant entry: {entry.data}")
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Unload a config entry."""
-    if await hass.config_entries.async_forward_entry_unload(entry, "light"):
-        hass.data[DOMAIN].pop(entry.entry_id)
-        return True
-    return False
+async def async_unload_entry(hass: HomeAssistant, entry):
+    """Unload Light Assistant config entry."""
+    _LOGGER.debug(f"Unloading Light Assistant entry: {entry.data}")
+    return True
